e350a82eb8e918c26458d73098954399715c3e32
ci <ci@banuba.com>
2024-01-11 12:36:21 +0300

    1.10.1

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 // swift-tools-version:5.3
 import PackageDescription
 
-let bnbPackageVersion: Version = "1.10.0-24-gb831098a87"
+let bnbPackageVersion: Version = "1.10.1"
 
 let package = Package(
     name: "BNBPoseEstimation",
@@ -36,8 +36,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "BNBPoseEstimation",
-            url: "https://d2cm7wtcqqw29.cloudfront.net/1.10.0-24-gb831098a87/BNBPoseEstimation.zip",
-            checksum: "6e560d022ade8e388a8f3c762c99cda8be47945ce6d8d090e21f3c6675645b7f"
+            url: "https://d2cm7wtcqqw29.cloudfront.net/1.10.1/BNBPoseEstimation.zip",
+            checksum: "9d61c657c7a8b1b87e1b2dedc546d1b4fd7384eb381762930497ed6bbb154186"
         ),
         .target(
             name: "BNBPoseEstimation_BNBSdkCore",
